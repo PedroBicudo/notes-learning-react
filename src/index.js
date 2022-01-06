72a15b5a18e7762c16f582e68fb80557832a7435
Pedro Bicudo <pedrobicudoa@gmail.com>
2022-01-06 17:39:29 -0300

    Adiciona as modificacoes da anotacoes sobre rendering elements

--- src/index.js
+++ src/index.js
@@ -4,12 +4,21 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+let value=0;
+function tick() {
+  return (
+    <div>
+      {value++}
+    </div>
+  );
+}
+
+setInterval(() => {
+  ReactDOM.render(
+    tick(),
+    document.getElementById('root')
+  );
+}, 1000);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
